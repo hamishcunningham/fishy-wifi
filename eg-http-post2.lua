6dde1c10ea4aa061522c6d3d24389a8057dcef58
Hamish Cunningham <hamish@gate.ac.uk>
2015-01-10 22:35:15 +0000

    indentation

--- eg-http-post2.lua
+++ eg-http-post2.lua
@@ -18,15 +18,16 @@ srv=net.createServer(net.TCP) srv:listen(80,function(conn)
     print(payload)
 
     function ctrlpower()
-    pwm.close(outpin)
-    gpio.mode(outpin,gpio.OUTPUT)
-    dotaz=string.sub(payload,kdesi[2]+1,#payload)
-    if dotaz=="ON"  then gpio.write(outpin,gpio.HIGH)return end
-    if dotaz=="OFF" then gpio.write(outpin,gpio.LOW)return end
-    if dotaz=="FLC" then pwm.setup(outpin,2,512)pwm.start(outpin)return end
-    pwm.setup(outpin,1000,dotaz*10)
-    pwm.start(outpin)
+      pwm.close(outpin)
+      gpio.mode(outpin,gpio.OUTPUT)
+      dotaz=string.sub(payload,kdesi[2]+1,#payload)
+      if dotaz=="ON"  then gpio.write(outpin,gpio.HIGH)return end
+      if dotaz=="OFF" then gpio.write(outpin,gpio.LOW)return end
+      if dotaz=="FLC" then pwm.setup(outpin,2,512)pwm.start(outpin)return end
+      pwm.setup(outpin,1000,dotaz*10)
+      pwm.start(outpin)
     end
+
     --parse position POST value from header
     kdesi={string.find(payload,"pwmi=")}
     --If POST value exist, set LED power
