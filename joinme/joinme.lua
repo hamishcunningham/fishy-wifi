02851fec6f69ee184ebf3f8b7be7afa28cbfadf3
Hamish Cunningham <hamish@gate.ac.uk>
2015-01-11 20:52:42 +0000

    bugs, bugs, everywhere, and never a drop to drink

--- joinme/joinme.lua
+++ joinme/joinme.lua
@@ -8,38 +8,40 @@ print("are we running on NodeMCU? ", nu.isnodemcu())
 
 -- config data
 local conffile = "joinmeconf.lua"
-local conf = nil
 
 -- local utilities
 local function printip() print("Wifi station ip: ", wifi.sta.getip()) end
-local function sayhi()
-  p("Fishy wifi up and swimming...")
-  p('MAC: %s; chip: %s; heap: %s',
-    wifi.sta.getmac(), node.chipid(), node.heap())
-  printip()
-end
 local function conf2string(conf)
   buf = "{\n"
   for k, v in pairs(conf) do
-    buf = buf .. string.format("  %s = %s\n", k, v)
+    buf = buf .. string.format('  %s = "%s",\n', k, v)
   end
   buf = buf .. "}\n"
   return buf
 end
 
 -- exports
-local function joinme.p(fmt, ...) return print(string.format(fmt, ...)) end
-local function joinme.getconf() return dofile(conffile) end
-local function joinme.writeconf(conf)
+function joinme.p(fmt, ...) return print(string.format(fmt, ...)) end
+function joinme.sayhi()
+  joinme.p("Fishy wifi up and swimming...")
+  joinme.p('MAC: %s; chip: %s; heap: %s',
+    wifi.sta.getmac(), node.chipid(), node.heap())
+  printip()
+end
+function joinme.getconf() 
+  status, results = pcall(dofile, conffile)
+  if status then return results else return nil end
+end
+function joinme.writeconf(conf)
   f = file.open(conffile, "w")
   if not f then return nil end
-  file.write("return " .. conf2str())
+  file.write("return " .. conf2string(conf))
   file.close()
   return true
 end
-local function joinme.joinwifi(conf)
-  p("config incoming:")
-  p(conf2string())
+function joinme.joinwifi(conf)
+  joinme.p("config incoming:")
+  joinme.p(conf2string(conf))
   wifi.setmode(wifi.STATION)
   wifi.sta.config(conf.ssid, conf.key)
   wifi.sta.connect()
--- joinme/nmutils.lua
+++ joinme/nmutils.lua
@@ -5,17 +5,35 @@
 nmutils = {}
 
 -- are we running on NodeMCU? if not then emulate...
-local function nmutils.isnodemcu()
+function nmutils.isnodemcu()
   return type(node) == "romtable" and type(node.info()) == "number"
 end
-if not nmutils.isnodemcu() -- define emulation functions
+if not nmutils.isnodemcu() -- define (quick and dirty!) emulation functions
 then
   node = {}
-  function node.info() return -1, -1, -1, -1, -1, -1, -1, -1 end
+  function node.info()          return -1, -1, -1, -1, -1, -1, -1, -1 end
+  function node.chipid()        return "|chip id|" end
+  function node.heap()          return "|heap|" end
+  wifi = {}
+  function wifi.setmode(m)      return "|wifi.setmode|" end
+  wifi.STATION = 1
+  wifi.SOFTAP = 2
+  wifi.STATIONAP = 3
+  wifi.sta = {}
+  function wifi.sta.getmac()    return "|a mac address|" end
+  function wifi.sta.getip()     return "|an ip address|" end
+  function wifi.sta.config(s,k) return "|wifi.sta.config|" end
+  function wifi.sta.connect()   return "|wifi.sta.connect|" end
+  file = {}
+  function file.write(s)        return io.write(s) end
+  function file.open(fname)     return io.output(fname) end
+  function file.close()         return io.close(io.output()) end
+  tmr = {}
+  function tmr.alarm(...)       return "|tmr.alarm " .. ... .. "|" end
 end
 
 -- utils
-local function nmutils.version() -- what version of NodeMCU is this?
+function nmutils.version() -- what version of NodeMCU is this?
   ver1, ver2, ver3 = node.info()
   return string.format("%d.%d.%d", ver1, ver2, ver3)
 end
