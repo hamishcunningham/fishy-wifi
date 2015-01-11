e00663391daf49d9f509ede852bdf1094af29915
Hamish Cunningham <hamish@gate.ac.uk>
2015-01-11 18:31:44 +0000

    refactoring

--- joinme/init.lua
+++ joinme/init.lua
@@ -4,48 +4,13 @@
 -- libraries
 jm = require("joinme")
 
--- config data
-local CFILE = "fishyconf.lua"
-conf = nil -- not local as we want dofile to overwrite it
-
--- utilities
-function p(fmt, ...) return print(string.format(fmt, ...)) end
-function printip() print("Wifi station ip: ", wifi.sta.getip()) end
-function sayhi()
-  p("Fishy wifi up and swimming...")
-  p('MAC: %s; chip: %s; heap: %s',
-    wifi.sta.getmac(), node.chipid(), node.heap())
-  printip()
-end
-function getconf() dofile(CFILE); return conf end
-function writeconf()
-  f = file.open(CFILE, "w")
-  if not f then return nil end
-  file.write("conf = ")
-  file.write(conf2str())
-  file.close()
-end
-function conf2string()
-  buf = "{\n"
-  for k, v in pairs(conf) do
-    buf = buf .. string.format("  %s = %s\n", k, v)
-  end
-  buf = buf .. "}\n"
-  return buf
-end
-
 -- do the work
-sayhi()
-getconf()
+jm.sayhi()
+jm.getconf()
 if conf -- we are configured
 then
-  p("config incoming:")
-  p(conf2string())
-  wifi.setmode(wifi.STATION)
-  wifi.sta.config(conf.ssid, conf.key)
-  wifi.sta.connect()
-  tmr.alarm(0, 5000, 0, function() printip() end)
+  jm.joinwifi(conf)
 else    -- no config, assume first run
-  p("no config yet")
-  -- writeconf(conf)
+  jm.p("no config yet")
+  -- jm.writeconf(conf)
 end
--- joinme/joinme.lua
+++ joinme/joinme.lua
@@ -1,10 +1,48 @@
 -- joinme.lua
 -- do initial wifi config for NodeMCU over HTTP
 
+-- preliminaries
 joinme = {}
-
 nu = require("nmutils")
-
 print("are we running on NodeMCU? ", nu.isnodemcu())
+
+-- config data
+local conffile = "joinmeconf.lua"
+local conf = nil
+
+-- utilities
+local function p(fmt, ...) return print(string.format(fmt, ...)) end
+local function printip() print("Wifi station ip: ", wifi.sta.getip()) end
+local function sayhi()
+  p("Fishy wifi up and swimming...")
+  p('MAC: %s; chip: %s; heap: %s',
+    wifi.sta.getmac(), node.chipid(), node.heap())
+  printip()
+end
+local function getconf() return require(conffile) end
+local function writeconf(conf)
+  f = file.open(conffile, "w")
+  if not f then return nil end
+  file.write("joinmeconf = {}\njoinmeconf = ")
+  file.write(conf2str())
+  file.write("return joinmeconf")
+  file.close()
+end
+local function conf2string(conf)
+  buf = "{\n"
+  for k, v in pairs(conf) do
+    buf = buf .. string.format("  %s = %s\n", k, v)
+  end
+  buf = buf .. "}\n"
+  return buf
+end
+local function jm.joinwifi(conf)
+  jm.p("config incoming:")
+  jm.p(conf2string())
+  wifi.setmode(wifi.STATION)
+  wifi.sta.config(conf.ssid, conf.key)
+  wifi.sta.connect()
+  tmr.alarm(0, 5000, 0, function() printip() end)
+end
 
 return joinme
