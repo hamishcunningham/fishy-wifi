7dee2a06e1d8177c6e445fcf638808b7b90037f8
Hamish Cunningham <hamish@gate.ac.uk>
2015-01-12 22:57:16 +0000

    more

--- joinme/init.lua
+++ joinme/init.lua
@@ -4,6 +4,8 @@
 -- libraries
 jm = require("joinme")
 
+function prtbl(t) for k, v in pairs(t) do print(k, v) end end
+
 -- do the work
 jm.sayhi()
 conf = jm.getconf()
@@ -13,7 +15,15 @@ then
   jm.joinwifi(conf)
 else    -- no config, assume first run
   jm.p("no config yet... going to scan")
-  aplist = wifi.sta.getap(function(t) return t end)
+  wifi.setmode(wifi.STATION)
+  aptbl = {}
+  wifi.sta.getap(function(t) aptbl = t end)
+  -- tmr.alarm(0, 2000, 0, function() return end)
+  prtbl(aptbl)
+  aplist = {}
+  for k, v in pairs(aptbl) do table.insert(aplist, k) end
+  prtbl(aplist)
+  wifi.setmode(wifi.SOFTAP)
   print(jm.chooserpage(aplist))
   -- jm.writeconf(conf)
 end
