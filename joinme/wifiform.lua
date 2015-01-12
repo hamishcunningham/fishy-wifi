ec549269e57948939d8ee58bd80037b5b17d7f7a
Hamish Cunningham <hamish@gate.ac.uk>
2015-01-12 09:09:03 +0000

    a form template for the user to choose wifi AP

---
+++ joinme/wifiform.lua
@@ -0,0 +1,15 @@
+-- wifiform.lua
+-- a template form for choosing wifi AP and sending credentials
+
+return [=[
+<p>Choose a wifi access point to join:
+<form action="chooseap">
+  <ol>
+# for _, ssid in pairs{aplist} do
+  <li>SSID: <input type="radio" name="ssid" value="$(ssid)">$(ssid)<br/>
+# end
+  </ol>
+  <br/>
+  <input type="submit" value="Submit">
+</form></p>
+]=]
