4501f07ffbcb2609decd958b60ba487df8b2026d
Hamish Cunningham <hamish@gate.ac.uk>
2015-01-20 16:21:24 +0000

    debugging

--- freakaponics/init.lua
+++ freakaponics/init.lua
@@ -1,8 +1,8 @@
 -- init.lua: top level entry point for the freakaponics sensor/actuator board
 freak = require("freak")
 function needwifi(continuation)
-  return not continuation.taskdata.joinme.ssid
+  return not continuation.taskdata["joinme"].ssid
 end
 tasks = { "joinme", "sense", "report", "act", 5000 }
 precons = { joinme=needwifi } -- task preconditions
-freak.begin(tasks, precons) -- never returns but may trigger restarts
+-- freak.begin(tasks, precons) -- never returns but may trigger restarts
