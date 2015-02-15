d814722a45b446ca6fcd78203a65d2c493dddacf
Hamish Cunningham <hamish@gate.ac.uk>
2015-02-15 23:30:21 +0000

    final voltage calibration compromise (which agrees with the average of two multimeters and a PSU)

--- batterylevel/batterylevel.lua
+++ batterylevel/batterylevel.lua
@@ -12,7 +12,7 @@ bl.getvolts = function()
      for i=1,samples do
           result=result+adc.read(0)
      end
-     voltage=string.format("%.2f",result/samples*0.0055)
+     voltage=string.format("%.2f",result/samples*0.0056)
      return voltage
 end
 
