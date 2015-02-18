93a5bdce72bf746b59a4b637e12e87d960e905ad
Hamish Cunningham <hamish@gate.ac.uk>
2015-02-18 18:47:09 +0000

    started work on soil moisture reader

--- joinme/f.lua
+++ joinme/f.lua
@@ -4,6 +4,27 @@ f={}
 function f.run()
   -- TODO read sensors and report on MQTT; fiddle with actuators
   print("f.run...")             -- DEBUG
+
+  -- TODO experimental capacitative charge reading (not working?)
+  pin=1
+  pause=1600
+  readings={}
+  sum=0
+  for i=1,1000 do
+    gpio.mode(pin, gpio.OUTPUT)
+    gpio.write(pin, gpio.LOW)
+    tmr.wdclr()
+    tmr.delay(pause)
+
+    c=0
+    gpio.mode(pin, gpio.INPUT)
+    while gpio.read(pin) == gpio.LOW do
+      c = c + 1
+    end
+    sum = sum + c
+  end
+  print("cap reading: ", sum/1000)
+
   return nil
 end
 return f
