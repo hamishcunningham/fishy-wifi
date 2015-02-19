0b1300a0f2b2127950a243fddc397252e765c1a6
Hamish Cunningham <hamish@gate.ac.uk>
2015-02-19 19:37:55 +0000

    implemented new control routine

--- wegrow/bat.lua
+++ wegrow/bat.lua
@@ -1,13 +1,14 @@
--- bat.lua -- read supply voltage when used with a 100k/470k resistor divider
+-- bat.lua
+-- read supply voltage when used with a 100k/470k resistor divider
 -- for vcc - maximum is about 4.9v - tuned for 3AA cells
 bat = {}
-bat.volts = function()
-  local samples=100
-  local result=0
-  for i=1, samples do
-    result=result+adc.read(0)
-  end
-  voltage=string.format("%.2f", result/samples*0.0056)
+local function volts()
+  local samples = 100
+  local result = 0
+  for i=1, samples do result = result + adc.read(0) end
+  voltage = string.format("%.2f", result/samples * 0.0054)
+  print("battery level: ", voltage)     -- DEBUG
   return voltage
 end
+bat.run = function() return volts() end
 return bat
--- wegrow/init.lua
+++ wegrow/init.lua
@@ -1,19 +1,37 @@
--- init.lua: top level entry point for the freakaponics sensor/actuator board
+-- init.lua: top level entry point for the WeGrow sensor/actuator board
 
-wifi.setmode(wifi.STATIONAP)    -- coding around nil result to first call issue
-function lstf(t) if t then for k,_ in pairs(t) do print(k) end end end
-wifi.sta.getap(lstf)            -- consume nil result
+function run()
+  print("starting wifi setup, heap= ", node.heap()) -- DEBUG
+  if file.open("skipj.txt") then        -- we're already configured
+    print("skipj exists")               -- DEBUG
+    file.close()
+  else
+    j=require("j")
+    wifi.sta.getap(j.aplstn)            -- joinme entry point
+  end
+  print("wifi config finished, heap= ", node.heap()) -- DEBUG
 
-print("starting wifi setup, heap= ", node.heap()) -- DEBUG
-if file.open("skipj.txt") then  -- we're already configured
-  print("skipj exists")         -- DEBUG
+  print("setting up stepper...")        -- DEBUG
+  steps = { "bat", "soil" }
+  sleeptime = 3 -- in seconds
+  step=1
+  stepf="step.txt"
+  if file.open(stepf, "r") then step = 0 + file.read(); file.close() end
+  stepname = steps[step]
+
+  print("taking a step: ", stepname)    -- DEBUG
+  s = require(stepname)
+  s.run()
+
+  print("incrementing step number")     -- DEBUG
+  if step == #steps then step = 0 end
+  file.open(stepf, "w")
+  file.write(step + 1)
   file.close()
-  bat=require("bat")
-  print("battery level: ", bat.volts()) -- DEBUG
--- TODO tmr.alarm(0, 2000, 0, f.run)
--- f.run()
-else
-  j=require("j")
-  wifi.sta.getap(j.aplstn)      -- joinme entry point
-end -- wifi config
-print("wifi config finished, heap= ", node.heap()) -- DEBUG
+
+  print("sleeping for ", sleeptime, " secs...")
+  node.dsleep(sleeptime * 1000000)
+end
+
+print("panic now if you're in that sort of mood") -- DEBUG
+tmr.alarm(0, 1500, 0, run)              -- panic space!
