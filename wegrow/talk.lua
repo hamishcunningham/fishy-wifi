08faa527d2e2830547c967eedda901c33caf1a27
Hamish Cunningham <hamish@gate.ac.uk>
2015-02-26 14:25:15 +0000

    delete debug code; add convenience step.txt

--- wegrow/talk.lua
+++ wegrow/talk.lua
@@ -1,9 +1,12 @@
 -- talk.lua: exchange data with server over MQTT
 talk={}
+local function prtbl(t) for k, v in pairs(t) do print(k, v) end end
 function talk.run()
   print("talk.run...")                          -- DEBUG
---  t = recall()
---  forget()
+  t = recall()
+  print("talk: t=")
+  prtbl(t)
+  forget()
   return nil
 end
 return talk
