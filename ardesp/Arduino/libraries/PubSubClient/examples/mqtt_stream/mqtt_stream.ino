9aa0bff28b8b115536f99e6dd0366c575d1e7c5b
layerzerolabs <info@l0l.org.uk>
2015-07-08 14:11:15 +0100

    got mqtt working

---
+++ ardesp/Arduino/libraries/PubSubClient/examples/mqtt_stream/mqtt_stream.ino
@@ -0,0 +1,77 @@
+/*
+ Example of using a Stream object to store the message payload
+
+ Uses SRAM library: https://github.com/ennui2342/arduino-sram
+ but could use any Stream based class such as SD
+
+  - connects to an MQTT server
+  - publishes "hello world" to the topic "outTopic"
+  - subscribes to the topic "inTopic"
+*/
+
+#include <ESP8266WiFi.h>
+#include <PubSubClient.h>
+#include <SRAM.h>
+
+const char *ssid =	"xxxxxxxx";		// cannot be longer than 32 characters!
+const char *pass =	"yyyyyyyy";		//
+
+// Update these with values suitable for your network.
+IPAddress server(172, 16, 0, 2);
+
+SRAM sram(4, SRAM_1024);
+
+void callback(const MQTT::Publish& pub) {
+  sram.seek(1);
+
+  // do something with the message
+  for (uint8_t i = 0; i < pub.payload_len(); i++) {
+    sram.write(pub.payload()[i]);
+    Serial.write(pub.payload()[i]);
+  }
+
+  Serial.println();
+}
+
+WiFiClient wclient;
+PubSubClient client(wclient, server);
+
+void setup()
+{
+  // Setup console
+  Serial.begin(115200);
+  delay(10);
+  Serial.println();
+  Serial.println();
+
+  client.set_callback(callback);
+
+  WiFi.begin(ssid, pass);
+
+  int retries = 0;
+  while ((WiFi.status() != WL_CONNECTED) && (retries < 10)) {
+    retries++;
+    delay(500);
+    Serial.print(".");
+  }
+  if (WiFi.status() == WL_CONNECTED) {
+    Serial.println("");
+    Serial.println("WiFi connected");
+  }
+
+  if (client.connect("arduinoClient")) {
+    client.publish("outTopic","hello world");
+    client.subscribe("inTopic");
+  }
+
+  sram.begin();
+  sram.seek(1);
+  
+  Serial.begin(9600);
+}
+
+void loop()
+{
+  client.loop();
+}
+
---
+++ ardesp/mqtt_test/mqtt_test/mqtt_test.ino
@@ -0,0 +1,59 @@
+/*
+ Basic MQTT example 
+ 
+  - connects to an MQTT server
+  - publishes "hello world" to the topic "outTopic"
+  - subscribes to the topic "inTopic"
+*/
+
+#include <ESP8266WiFi.h>
+#include <PubSubClient.h>
+
+const char *ssid =	"bloogle";		// cannot be longer than 32 characters!
+const char *pass =	"2bigFish";		//
+
+// Update these with values suitable for your network.
+IPAddress server(10, 0, 0, 10);
+
+void callback(const MQTT::Publish& pub) {
+  // handle message arrived
+}
+
+WiFiClient wclient;
+PubSubClient client(wclient, server);
+
+void setup()
+{
+  // Setup console
+  Serial.begin(115200);
+  delay(10);
+  Serial.println();
+  Serial.println();
+
+  client.set_callback(callback);
+
+  WiFi.begin(ssid, pass);
+
+  int retries = 0;
+  while ((WiFi.status() != WL_CONNECTED) && (retries < 10)) {
+    retries++;
+    delay(500);
+    Serial.print(".");
+  }
+  if (WiFi.status() == WL_CONNECTED) {
+    Serial.println("");
+    Serial.println("WiFi connected");
+    Serial.println(WiFi.localIP());
+  }
+
+  if (client.connect("arduinoClient")) {
+    client.publish("outTopic","hello world!!");
+    client.subscribe("inTopic");
+  }
+}
+
+void loop()
+{
+  client.loop();
+}
+
