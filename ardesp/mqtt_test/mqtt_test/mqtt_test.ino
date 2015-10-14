c1049ade58f6b06047d800ec3f1242f538fcdf36
layerzerolabs <info@l0l.org.uk>
2015-10-14 10:51:28 +0100

    Added sucessful humidity test code - uses adafruit DHT library

--- ardesp/mqtt_test/mqtt_test/mqtt_test.ino
+++ ardesp/mqtt_test/mqtt_test/mqtt_test.ino
@@ -13,7 +13,7 @@ const char *ssid =	"bloogle";		// cannot be longer than 32 characters!
 const char *pass =	"2bigFish";		//
 
 // Update these with values suitable for your network.
-IPAddress server(10, 0, 0, 10);
+IPAddress server(10, 0, 0, 11);
 
 void callback(const MQTT::Publish& pub) {
   // handle message arrived
