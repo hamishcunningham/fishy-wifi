5bb199579fbc42ee71bd118773f1463b65ec1a65
layerzerolabs <info@l0l.org.uk>
2015-10-16 12:39:51 +0100

    reworked temp sensor code to use dallas library

--- ardesp/waterelf/waterelf.ino
+++ ardesp/waterelf/waterelf.ino
@@ -5,6 +5,7 @@
 #include <ESP8266WebServer.h>
 #include <DNSServer.h>
 #include <OneWire.h>
+#include <DallasTemperature.h>
 #include <RCSwitch.h>
 
 /////////////////////////////////////////////////////////////////////////////
@@ -79,9 +80,11 @@ void printMonitorEntry(monitor_t m, String* buf);
 /////////////////////////////////////////////////////////////////////////////
 // temperature sensor stuff /////////////////////////////////////////////////
 OneWire ds(2); // DS1820 on pin 2 (a 4.7K resistor is necessary)
+DallasTemperature tempSensor(&ds);
 void getTemperature(float* waterCelsius);
 const boolean GOT_TEMP_SENSOR = true;
-
+// array to hold device address
+DeviceAddress tempAddr;
 /////////////////////////////////////////////////////////////////////////////
 // RC switch stuff //////////////////////////////////////////////////////////
 RCSwitch mySwitch = RCSwitch();
@@ -388,9 +391,12 @@ void handle_actuate() {
 /////////////////////////////////////////////////////////////////////////////
 // sensor/actuator stuff ////////////////////////////////////////////////////
 void startPeripherals() {
-  // RC Transmitter is connected to Pin #13  
-  mySwitch.enableTransmit(13);
+  mySwitch.enableTransmit(13);   // RC Transmitter is connected to Pin #13  
+  tempSensor.begin();     // Start the onewire temperature sensor
+  tempSensor.getAddress(tempAddr, 0);
+  tempSensor.setResolution(tempAddr, 12);    // set the resolution to 12 bit (DS18B20 goes from 9-12 bit)
 }
+
 void updateSensorData(monitor_t *monitorData) {
   // Serial.print("monitorCursor = "); Serial.print(monitorCursor);
   // Serial.print(" monitorSize = ");  Serial.println(monitorSize);
@@ -436,90 +442,14 @@ void printMonitorEntry(monitor_t m, String* buf) {
   buf->concat("°C");
 }
 void getTemperature(float* waterCelsius) {
-  byte i;
-  byte present = 0;
-  byte type_s;
-  byte data[12];
-  byte addr[8];
   float _waterCelsius = *waterCelsius;
   Serial.println("getTemperature()...");
-
-  while(!ds.search(addr)) {
-    Serial.print("  no addresses; resetting. ");
-    ds.reset_search();
-    delay(250);
-  }
-
-  if(OneWire::crc8(addr, 7) != addr[7]) {
-      Serial.print("CRC is not valid!");
-      return;
-  }
-
-  // the first ROM byte indicates which chip
-  switch(addr[0]) {
-    case 0x10:
-      Serial.print("chip=DS18S20");  // or old DS1820
-      type_s = 1;
-      break;
-    case 0x28:
-      Serial.print("chip=DS18B20");
-      type_s = 0;
-      break;
-    case 0x22:
-      Serial.print("chip=DS1822");
-      type_s = 0;
-      break;
-    default:
-      Serial.print("device is not a DS18x20 family device :-(");
-      return;
-  }
-
-  ds.reset();
-  ds.select(addr);
-  ds.write(0x44, 1);    // start conversion, with parasite power on at the end
-
-  delay(1000);          // maybe 750ms is enough, maybe not
-  // we might do a ds.depower() here, but the reset will take care of it
-
-  present = ds.reset();
-  ds.select(addr);
-  ds.write(0xBE);       // read scratchpad
-
-  Serial.print("; data=");
-  Serial.print(present, HEX);
-  Serial.print(" ");
-  for(i = 0; i < 9; i++) { // we need 9 bytes
-    data[i] = ds.read();
-    Serial.print(data[i], HEX);
-    Serial.print(" ");
-  }
-  Serial.print("; CRC=");
-  Serial.print(OneWire::crc8(data, 8), HEX);
-
-  // convert the data to actual temperature
-  // because the result is a 16 bit signed integer, it should
-  // be stored to an "int16_t" type, which is always 16 bits
-  // even when compiled on a 32 bit processor
-  int16_t raw = (data[1] << 8) | data[0];
-  if(type_s) {
-    raw = raw << 3;     // 9 bit resolution default
-    if(data[7] == 0x10) {
-      // "count remain" gives full 12 bit resolution
-      raw = (raw & 0xFFF0) + 12 - data[6];
-    }
-  } else {
-    byte cfg = (data[4] & 0x60);
-    // at lower res, the low bits are undefined, so let's zero them
-    if(cfg == 0x00) raw = raw & ~7;  // 9 bit resolution, 93.75 ms
-    else if(cfg == 0x20) raw = raw & ~3; // 10 bit res, 187.5 ms
-    else if(cfg == 0x40) raw = raw & ~1; // 11 bit res, 375 ms
-    // default is 12 bit resolution, 750 ms conversion time
-  }
-  _waterCelsius = (float) raw / 16.0;
+  tempSensor.requestTemperatures(); // Send the command to get temperatures
+  Serial.println("DONE");
+  _waterCelsius = (tempSensor.getTempC(tempAddr));
   Serial.print("; temp: ");
   Serial.print(_waterCelsius);
   Serial.print(" C, ");
-
   *waterCelsius = _waterCelsius;
   return;
 }
