205368c41d97583e8e81291dd21c9a79005d3c0a
Hamish Cunningham <hamish@gate.ac.uk>
2016-04-07 13:35:59 +0100

    better docs; updating IDE

--- ardesp/ardesp-ide.sh
+++ ardesp/ardesp-ide.sh
@@ -5,20 +5,23 @@
 # standard locals
 alias cd='builtin cd'
 P="$0"
-USAGE="`basename ${P}` [-h(elp)] [-d(ebug)] [-S(taging)] [-H(EAD)]"
+USAGE="`basename ${P}` [-h(elp)] [-d(ebug)] [-S(taging)] [-H(EAD)] [-8 (1.6.8)]"
 DBG=:
-OPTIONSTRING=hdSHx:
+OPTIONSTRING=hdSHx:8
 
 # specific locals
 IDEBASE=~/esp-arduino-ide
 PREFSDIR=~/.arduino15
 SPREFSDIR=${IDEBASE}/dot-arduino15-staging
 HPREFSDIR=${IDEBASE}/dot-arduino15-head
+EIGHTPREFSDIR=${IDEBASE}/dot-arduino15-168
 X=
 STAGING=
 HEAD=
+EIGHT=
 STAGINGDIR=${IDEBASE}/arduino-1.6.5-r5
 HEADDIR=${IDEBASE}/arduino-1.6.5-r5--local-esp
+EIGHTDIR=${IDEBASE}/arduino-1.6.8
 
 # message & exit if exit num present
 usage() { echo -e Usage: $USAGE; [ ! -z "$1" ] && exit $1; }
@@ -31,6 +34,7 @@ do
     d)	DBG=echo ;;
     S)	STAGING=yes ;;
     H)	HEAD=yes ;;
+    8)	EIGHT=yes ;;
     x)	X="${OPTARG}" ;;
     *)	usage 1 ;;
   esac
@@ -38,9 +42,9 @@ done
 shift `expr $OPTIND - 1`
 
 # need to specify a version to run
-if [ x$STAGING == x -a x$HEAD == x ]
+if [ x$STAGING == x -a x$HEAD == x -a x$EIGHT == x ]
 then
-  echo you must choose either staging or head
+  echo you must choose staging or head or 8
   usage 2
 fi
 
@@ -53,6 +57,10 @@ elif [ x$HEAD == xyes ]
 then
   USEPREFS=${HPREFSDIR}
   USEIDE=${HEADDIR}
+elif [ x$EIGHT == xyes ]
+then
+  USEPREFS=${EIGHTPREFSDIR}
+  USEIDE=${EIGHTDIR}
 else
   echo you must choose either staging or head
   usage 3
