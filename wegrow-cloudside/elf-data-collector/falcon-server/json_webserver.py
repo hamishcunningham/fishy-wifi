9a494c8723ce6af07c845131aaae996351addead
Zac Rubin <zac.delta56@gmail.com>
2016-04-29 13:16:57 +0100

    New JSON accepting webserver with accompanying middleware.

--- wegrow-cloudside/elf-data-collector/falcon-server/json_webserver.py
+++ wegrow-cloudside/elf-data-collector/falcon-server/json_webserver.py
@@ -2,27 +2,19 @@ import falcon
 import json
 import datetime
 import os
+from middleware import RequireJSON, JSONTranslator
 
 
-class ParameterWaterElfCollector:
-    """Accept data via HTTP parameters"""
+class JSONWaterElfCollector:
+    """Accepts JSON encoded data in the request body"""
     # The keys should be the same as the keys output by the WaterElf
-    # Used for validation and processing
+    # Used for validation 
     KEY_TIMESTAMP = "timestamp"
     KEY_WATER_TEMP = "waterTemp"
     KEY_AIR_TEMP= "airTemp"
     KEY_PH = "pH"
     KEY_HUMIDITY = "humidity"
     KEY_LUX = "lux"
-    # There's probably a more elegant way to do this.
-    # I am sorry.
-    TYPES = dict()
-    TYPES[KEY_TIMESTAMP] = int
-    TYPES[KEY_WATER_TEMP] = float
-    TYPES[KEY_AIR_TEMP] = float
-    TYPES[KEY_PH] = float
-    TYPES[KEY_HUMIDITY] = float
-    TYPES[KEY_LUX] = int
     
     def __init__(self):
         self.file_ext = "txt"
@@ -32,11 +24,10 @@ class ParameterWaterElfCollector:
         self.required_keys = None
 
     def on_get(self, req, resp, uuid):
-        # Accept data from the parameters as a dict
-        data = req.params
+        # Accept JSON data from the body as a dict via middleware
+        data = req.context['json_input']
 
         self.check_data(data)
-        data = self.process_data(data)
         output_path = self.generate_path(uuid)
         self.write_data_line(output_path, json.dumps(data)) 
 
@@ -74,13 +65,8 @@ class ParameterWaterElfCollector:
                 if key not in data.keys():
                     raise falcon.HTTPMissingParam(key)
 
-    def process_data(self, data):
-        output = dict()
-        for key, val in data.iteritems():
-            output[key] = self.TYPES[key](val)            
-        return output
-
-
-api = falcon.API(middleware=[])
-api.add_route("/collect/{uuid}", ParameterWaterElfCollector())
+# Middleware enforces the requirement for 'application/json' in 'content-type' and 'accept'.
+# Middleware decodes JSON data into a python object
+api = falcon.API(middleware=[RequireJSON(), JSONTranslator()])
+api.add_route("/collect/{uuid}", JSONWaterElfCollector())
 
