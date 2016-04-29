c455d02f5661ac87f6934a8a1076755c3b9ffd7e
Hamish Cunningham <hamish@gate.ac.uk>
2016-04-29 12:52:34 +0100

    added some config

--- wegrow-web/pelicanconf.py
+++ wegrow-web/pelicanconf.py
@@ -3,33 +3,43 @@
 from __future__ import unicode_literals
 
 AUTHOR = u'Hamish Cunningham'
-SITENAME = u'WeGrow WaterElf: Clean Local Food, Global Community'
+SITENAME = u'WeGrow WaterElf: Local Food, Global Community'
 SITEURL = ''
-
 PATH = 'content'
-
-TIMEZONE = 'Europe/Paris'
-
+TIMEZONE = 'Europe/London'
 DEFAULT_LANG = u'en'
-
-# Feed generation is usually not desired when developing
 FEED_ALL_ATOM = None
 CATEGORY_FEED_ATOM = None
 TRANSLATION_FEED_ATOM = None
 AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
-
-# Blogroll
-LINKS = (('Pelican', 'http://getpelican.com/'),
-         ('Python.org', 'http://python.org/'),
-         ('Jinja2', 'http://jinja.pocoo.org/'),
-         ('You can modify those links in your config file', '#'),)
-
-# Social widget
-SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
-
 DEFAULT_PAGINATION = 10
 
-# Uncomment following line if you want document-relative URLs when developing
-#RELATIVE_URLS = True
+MARKUP = ('rst', 'md', 'html')
+STATIC_PATHS = ['images', 'pages/images', '.htaccess', '.htpasswd']
+FILENAME_METADATA = ('(?P<date>\d{4}-\d{2}-\d{2}).*')
+PAGE_EXCLUDES = ([])
+ARTICLE_EXCLUDES = (['pages'])
+MONTH_ARCHIVE_SAVE_AS = 'posts/{date:%Y}/{date:%m}/index.html'
+YEAR_ARCHIVE_SAVE_AS = 'posts/{date:%Y}/index.html'
+ARTICLE_URL = 'posts/{date:%Y}/{date:%m}/{date:%d}/{slug}/'
+ARTICLE_SAVE_AS = 'posts/{date:%Y}/{date:%m}/{date:%d}/{slug}/index.html'
+SHOW_ARTICLE_AUTHOR = True
+
+#THEME = 'theming/original'
+THEME='theming/pelican-bootstrap3'
+#BOOTSTRAP_THEME='united'
+#BOOTSTRAP_THEME='cyborg'
+#BOOTSTRAP_THEME='darkly'
+BOOTSTRAP_THEME='readable'
+FAVICON='favicon.ico'
+DISPLAY_PAGES_ON_MENU = False
+DISPLAY_CATEGORIES_ON_MENU = False
+DISPLAY_TAGS_ON_SIDEBAR = False
+DISPLAY_ARTICLE_INFO_ON_INDEX = False
+HIDE_SITENAME = True
+ARTICLE_ORDER_BY = 'reversed-date'
+TWITTER_USERNAME = 'hcunningham'
+GOOGLE_ANALYTICS = 'TODO UA-41812045-2'
+DISQUS_SITENAME = 'wegrow'
+DISQUS_DOMAIN = 'https://wegrow.social'
