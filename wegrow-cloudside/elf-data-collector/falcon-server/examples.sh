86a7cc11b53eface5e3e4d7a195716174fd350dc
Hamish Cunningham <hamish@gate.ac.uk>
2016-06-22 09:59:42 +0100

    better debug script;

--- wegrow-cloudside/elf-data-collector/falcon-server/examples.sh
+++ wegrow-cloudside/elf-data-collector/falcon-server/examples.sh
@@ -1,35 +1,27 @@
 #!/bin/bash
 
-# nc file-based raw req version with no trailing newline (not working)
-cat req.txt | awk 'NR>1{print PREV} {PREV=$0} END{printf("%s",$0)}' | \
-  nc localhost 8000
-exit $?
-
-# curl parameterised version
-curl -i -H "Accept: application/json" -H "Content-Type: application/json" \
-  -d '{"timestamp": 999888777, "airTemp": 14, "humidity": 44, "lux": 1494, "pH": 6.4, "waterTemp": 14}' \
-  -X GET 'localhost:8000/collect/9999'
-exit $?
-
-# nc file-based raw req version (not working)
-nc localhost 8000 <req.txt
-exit $?
-
-# curl raw request version -- probably doesn't send a valid request :-(
+# nc raw request version (working)
 cat <<EOF |nc localhost 8000
 GET /collect/9999 HTTP/1.1
 User-Agent: curl/7.35.0
 Host: localhost:8000
 Accept: application/json
 Content-Type: application/json
-Content-Length: 91
+Content-Length: 92
 
 {"timestamp": 1000, "airTemp": 14, "humidity": 44, "lux": 1494, "pH": 6.4, "waterTemp": 14}
 EOF
 exit $?
 
-# curly file-based raw req version -- doesn't send a valid request :-(
-curl 'localhost:8000' <req.txt
+# nc file-based raw req version with no trailing newline (working)
+cat req.txt | awk 'NR>1{print PREV} {PREV=$0} END{printf("%s",$0)}' | \
+  nc localhost 8000
+exit $?
+
+# curl parameterised version (working)
+curl -i -H "Accept: application/json" -H "Content-Type: application/json" \
+  -d '{"timestamp": 999888777, "airTemp": 14, "humidity": 44, "lux": 1494, "pH": 6.4, "waterTemp": 14}' \
+  -X GET 'localhost:8000/collect/9999'
 exit $?
 
 
