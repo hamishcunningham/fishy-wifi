b87fd86a57c718649ed5318eec7132848b934039
Hamish Cunningham <hamish@gate.ac.uk>
2017-01-06 13:50:26 +0000

    more

--- walls/rs485-api.sh
+++ walls/rs485-api.sh
@@ -27,25 +27,61 @@ do
 done 
 shift `expr $OPTIND - 1`
 
-# TODO
 doit() {
   while :
   do
     echo turn it on...
-    echo -e '\x55\xaa\x0B\x24\xFE\x00\xFF\x03\xFF\x01\x00\x2F\x77' > /dev/ttyUSB0
+    echo -e "\x55\xAA\x0D\x10\x00\x01\x00\x00\x00\x00\x00\x00\x00\x20\x77" > /dev/ttyUSB0 
     sleep 2
     echo turn it off
-    echo -e '\x55\xaa\x0B\x24\xFE\x00\x00\x00\xFF\x01\x00\x2D\x77' > /dev/ttyUSB0
+    echo -e "\x55\xAA\x0D\x10\x00\x00\x00\x00\x00\x00\x00\x00\x00\x1F\x77" > /dev/ttyUSB0
+
     sleep 2
   done
 }
 
-# doit
-#echo doing "cat </dev/ttyUSB0"; cat </dev/ttyUSB0
-echo sending request
-echo "echo -e '\x55\xaa\x05\x10\xFE\x13\x77' >/dev/ttyUSB0"
-echo -e '\x55\xaa\x05\x10\xFE\x13\x77' >/dev/ttyUSB0
-#sleep 1
-#echo doing "cat </dev/ttyUSB0"; cat </dev/ttyUSB0
-#echo doing tail -f
-#tail -f /dev/ttyUSB0
+doit2() {
+  echo sending request
+  echo 'echo -e '\x55\xAA\x05\x10\xFE\x13\x77' >/dev/ttyUSB0'
+  echo -e '\x55\xAA\x05\x10\xFE\x13\x77' >/dev/ttyUSB0
+  #sleep 1
+  #echo doing "cat </dev/ttyUSB0"; cat </dev/ttyUSB0
+  #echo doing tail -f
+  #tail -f /dev/ttyUSB0
+}
+
+init() {
+  echo init
+  stty -F /dev/ttyUSB0 sane
+  stty -F /dev/ttyUSB0 9600 cs8 -cstopb -parenb raw -echo
+}
+
+tellme() {
+  echo tellme
+  od -t x1 -N13 < /dev/ttyUSB0 &2>od-out.txt &
+  sleep 1
+  echo -e '\x55\xAA\x05\x0F\xFE\x14\x77' >/dev/ttyUSB0
+  cat od-out.txt
+}
+
+set -x
+init
+doit
+#tellme
+
+
+#The -F switch says to configure the /dev/ttyUSB0 port (I am using a cheap USB RS485 transceiver for testing)
+#
+#9600 is the baud, cs8 is 8 bits of data, -cstopb is 1 stop bit, -parenb is no parity.
+#
+#raw tells linux not to process the input per line, and -echo stops it echoing.
+#
+#Then I can use the command:
+#
+#od -t x1 -N13 < /dev/ttyUSB0
+#
+#the N13 says to stop after 13 bytes.
+#
+#If I run the od command in one terminal and in another:
+#
+#echo -e "\x55\xAA\x05\x0F\xFE\x14\x77" > /dev/ttyUSB0
