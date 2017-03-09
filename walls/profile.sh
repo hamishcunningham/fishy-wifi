8afd2922672d8b634de25dba1605db7ec40da4cc
Hamish Cunningham <hamish@gate.ac.uk>
2017-03-09 12:12:47 +0000

    more

--- walls/profile.sh
+++ walls/profile.sh
@@ -1,6 +1,8 @@
 # stuff this in /home/pi/.profile and light blue touch paper
 # (pi should be configured to autologin to console)
 
+PS1='(to restart type "sudo reboot")  '"${PS1}"
+
 /home/pi/fishy-wifi/walls/tui.sh
 echo -n "Shutdown? (s) Reboot? (r) Shell? (any) "
 read ANS
