dc1ae56a33ca7a5802bb7590f600b5d4ff8cb3a2
Gareth Coleman <info@l0l.org.uk>
2017-05-08 12:57:41 +0100

    added current_test sketch using non-invasive current sensor, deleted structured version

---
+++ ardesp/current_test/current_test.ino
@@ -0,0 +1,19 @@
+// EmonLibrary examples openenergymonitor.org, Licence GNU GPL V3
+
+#include "EmonLib.h"                   // Include Emon Library
+EnergyMonitor emon1;                   // Create an instance
+
+void setup()
+{  
+  Serial.begin(115200);
+  
+  emon1.current(A0, 111.1);             // Current: input pin, calibration.
+}
+
+void loop()
+{
+  double Irms = emon1.calcIrms(1480);  // Calculate Irms only
+  
+  Serial.print("Current: ");
+  Serial.println(Irms);          // Irms
+}
