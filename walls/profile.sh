533e80870548e7c26cc966e24863a1a32350c6c3
Hamish Cunningham <hamish@gate.ac.uk>
2017-05-17 15:23:17 +0100

    run the leak trap on startup

--- walls/profile.sh
+++ walls/profile.sh
@@ -5,6 +5,8 @@ PS1='(to restart type "sudo reboot")  '"${PS1}"
 
 if [ `tty` = "/dev/tty1" ]
 then
+  /home/pi/fishy-wifi/walls/rs485-api.sh -c trap_leaks_and_kill_pump \
+    >>$DBG_LOG 2>&1 &
   /home/pi/fishy-wifi/walls/tui.sh
   echo -n "Shutdown? (s) Reboot? (r) Shell? (any) "
   read ANS
