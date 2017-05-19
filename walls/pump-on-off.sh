5ebbef8741dd387150a5a41f968d134aa333f6d3
Hamish Cunningham <hamish@gate.ac.uk>
2017-05-19 11:16:03 +0100

    cron control of pump

--- walls/pump-on-off.sh
+++ walls/pump-on-off.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
-# CLI API for the STR2DO14DIN RS-485 controller
+# ask the pump controller Elf to turn the pump on or off
 
 FISH_ELF_IP=192.168.1.201
-
-          curl -v "http://${FISH_ELF_IP}/actuate" \
-            -H "Origin: http://${FISH_ELF_IP}" \
-            -H 'Accept-Encoding: gzip, deflate' \
-            -H 'Accept-Language: en-GB,en-US;q=0.8,en;q=0.6' \
-            -H 'Upgrade-Insecure-Requests: 1' \
-            -H 'User-Agent: curl' \
-            -H 'Content-Type: application/x-www-form-urlencoded' \
-            -H 'Accept: text/html,application/xhtml+xml,application/xml' \
-            -H 'Cache-Control: max-age=0' \
-            -H "Referer: http://${FISH_ELF_IP}/" \
-            -H 'Connection: keep-alive' \
-            --data "state=$1" --compressed
+/usr/bin/curl \
+  -v "http://${FISH_ELF_IP}/actuate" \
+  -H "Origin: http://${FISH_ELF_IP}" \
+  -H 'Accept-Encoding: gzip, deflate' \
+  -H 'Accept-Language: en-GB,en-US;q=0.8,en;q=0.6' \
+  -H 'Upgrade-Insecure-Requests: 1' \
+  -H 'User-Agent: curl' \
+  -H 'Content-Type: application/x-www-form-urlencoded' \
+  -H 'Accept: text/html,application/xhtml+xml,application/xml' \
+  -H 'Cache-Control: max-age=0' \
+  -H "Referer: http://${FISH_ELF_IP}/" \
+  -H 'Connection: keep-alive' \
+  --data "state=$1" --compressed
