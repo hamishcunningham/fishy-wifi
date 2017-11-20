fd9b2b81eb8a37469b1c9962a707e193b5200367
Euan Rochester <euan@rochester.me.uk>
2017-11-20 12:47:34 +0000

    Fix overlong line lengths from PR#19

--- ardesp/waterelf32/joinme.cpp
+++ ardesp/waterelf32/joinme.cpp
@@ -7,19 +7,24 @@
 #include <WiFiClient.h>
 #include <DNSServer.h>
 
-//these 2 includes required to change default dhcps behaviour until https://github.com/espressif/esp-idf/issues/1285 is resolved
+//these 2 includes required to change default dhcps behaviour until
+//https://github.com/espressif/esp-idf/issues/1285 is resolved
 #include <apps/dhcpserver_options.h>
 #include <tcpip_adapter.h>
 
 const byte DNS_PORT = 53;
-//307 is temporary redirect. if we used 301 we'd probably break the user's browser for sites they were captured from until they cleared their cache
+//307 is temporary redirect. if we used 301 we'd probably break the user's
+//browser for sites they were captured from until they cleared their cache
 int TEMPORARY_REDIRECT = 307;
 DNSServer dnsServer;
 ESPWebServer* server_;
 IPAddress apIP_;
 
 void do_redirect(){
-  Serial.printf("joinme redirecting captured client to: %s",apIP_.toString().c_str());
+  Serial.printf(
+    "joinme redirecting captured client to: %s",
+    apIP_.toString().c_str()
+  );
   server_->sendHeader("Location","http://"+apIP_.toString()+"/");
   server_->send(TEMPORARY_REDIRECT,"text/plain","");
 }
@@ -37,18 +42,27 @@ void handle_ALL() {
 }
 
 void joinme_dhcps_hack(){
-  //Do not use a custom dns server, which (possibly due to a bug) we can't even set
-  //instead use the default, which is the gateway (in this case us)
-  //may be removed if https://github.com/espressif/esp-idf/issues/1285 is resolved
+  //Do not use a custom dns server, which (possibly due to a bug) we can't even
+  //set. Instead use the default, which is the gateway (in this case us)
+  //may be removed if
+  //https://github.com/espressif/esp-idf/issues/1285 is resolved
   uint8_t no = 0;
-  tcpip_adapter_dhcps_option(TCPIP_ADAPTER_OP_SET,(tcpip_adapter_option_id_t) DOMAIN_NAME_SERVER,(void *) &no, sizeof(dhcps_offer_t));
+  tcpip_adapter_dhcps_option(
+    TCPIP_ADAPTER_OP_SET,
+    (tcpip_adapter_option_id_t) DOMAIN_NAME_SERVER,
+    (void *) &no,
+    sizeof(dhcps_offer_t)
+  );
 }
 
 void joinme_setup(ESPWebServer* server, IPAddress apIP) {
   assert(server != NULL);
   server_ = server;
   apIP_ = apIP;
-  Serial.printf("joinme will direct captured clients to: %s\n",apIP_.toString().c_str());
+  Serial.printf(
+    "joinme will direct captured clients to: %s\n",
+    apIP_.toString().c_str()
+  );
   dnsServer.setErrorReplyCode(DNSReplyCode::NoError);
   dnsServer.start(DNS_PORT, "*", apIP_);
   Serial.println("joinme captive dns server started");
